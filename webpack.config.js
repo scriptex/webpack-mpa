c5b3f93b8f05b0750dd2b0b0e8524eba00d21a6f
Atanas Atanasov <scriptex.bg@gmail.com>
2018-01-26 23:37:39 +0200

    Change indentation

--- webpack.config.js
+++ webpack.config.js
@@ -6,164 +6,168 @@ const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 const paths = {
-  stylesSrc: './assets/src/styles/main.scss',
-  stylesBuild: 'assets/styles/app.css',
-  scriptsSrc: './assets/src/scripts/main.js',
-  scriptsBuild: './assets/scripts/app.js',
-  resolveModules: 'node_modules',
-  resolveScripts: './assets/scripts',
-  resolveIcons: './assets/images/sprite',
-  iconsSrc: 'assets/images/sprite',
-  iconsTarget: 'assets/images/sprite.png',
-  iconsStyle: 'assets/src/styles/_sprite.scss',
-  iconsRef: '../../images/sprite.png',
-  cleanUp: [
-    './assets/scripts/',
-    './assets/styles/',
-    './assets/images/sprite.png',
-    './sprite.png'
-  ]
+	stylesSrc: './assets/src/styles/main.scss',
+	stylesBuild: 'assets/styles/app.css',
+	scriptsSrc: './assets/src/scripts/main.js',
+	scriptsBuild: './assets/scripts/app.js',
+	resolveModules: 'node_modules',
+	resolveScripts: './assets/scripts',
+	resolveIcons: './assets/images/sprite',
+	iconsSrc: 'assets/images/sprite',
+	iconsTarget: 'assets/images/sprite.png',
+	iconsStyle: 'assets/src/styles/_sprite.scss',
+	iconsRef: '../../images/sprite.png',
+	cleanUp: [
+		'./assets/scripts/',
+		'./assets/styles/',
+		'./assets/images/sprite.png',
+		'./sprite.png'
+	]
 };
 
 const postcssConfig = {
-  plugins: [
-    require('postcss-easy-import'),
-    require('postcss-flexbugs-fixes'),
-    require('postcss-utilities'),
-    require('postcss-merge-rules'),
-    require('autoprefixer')(),
-    require('cssnano')({
-      discardComments: {
-        removeAll: true
-      }
-    })
-  ],
-  sourceMap: true
+	plugins: [
+		require('postcss-easy-import'),
+		require('postcss-flexbugs-fixes'),
+		require('postcss-utilities'),
+		require('postcss-merge-rules'),
+		require('autoprefixer')(),
+		require('cssnano')({
+			discardComments: {
+				removeAll: true
+			}
+		})
+	],
+	sourceMap: true
 };
 
 const babelConfig = [
-  {
-    loader: 'babel-loader',
-    options: {
-      cacheDirectory: true,
-      comments: false,
-      presets: ['env', 'stage-2']
-    }
-  }
+	{
+		loader: 'babel-loader',
+		options: {
+			cacheDirectory: true,
+			comments: false,
+			presets: ['env', 'stage-2']
+		}
+	}
 ];
 
 const config = {
-  entry: [paths.stylesSrc, paths.scriptsSrc],
-  output: {
-    filename: paths.scriptsBuild
-  },
-  resolve: {
-    modules: [paths.resolveModules, paths.resolveScripts, paths.resolveIcons]
-  },
-  module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        use: ExtractTextPlugin.extract({
-          use: [
-            {
-              loader: 'css-loader',
-              options: {
-                sourceMap: true
-              }
-            },
-            {
-              loader: 'postcss-loader',
-              options: postcssConfig
-            },
-            {
-              loader: 'sass-loader',
-              options: {
-                sourceMap: true
-              }
-            }
-          ],
-          fallback: 'style-loader'
-        })
-      },
-      {
-        test: /\.png$/,
-        loaders: ['file-loader?name=../../[path][name].[ext]']
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: babelConfig
-      }
-    ]
-  },
-  plugins: [
-    new ExtractTextPlugin({
-      filename: paths.stylesBuild,
-      allChunks: true
-    }),
-    new SpritesmithPlugin({
-      src: {
-        cwd: path.resolve(__dirname, paths.iconsSrc),
-        glob: '*.png'
-      },
-      target: {
-        image: path.resolve(__dirname, paths.iconsTarget),
-        css: path.resolve(__dirname, paths.iconsStyle)
-      },
-      apiOptions: {
-        cssImageRef: paths.iconsRef
-      }
-    }),
-    new CleanWebpackPlugin(paths.cleanUp, {
-      verbose: false
-    })
-  ],
-  cache: true,
-  bail: false,
-  devtool: 'source-map'
+	entry: [paths.stylesSrc, paths.scriptsSrc],
+	output: {
+		filename: paths.scriptsBuild
+	},
+	resolve: {
+		modules: [
+			paths.resolveModules,
+			paths.resolveScripts,
+			paths.resolveIcons
+		]
+	},
+	module: {
+		rules: [
+			{
+				test: /\.scss$/,
+				use: ExtractTextPlugin.extract({
+					use: [
+						{
+							loader: 'css-loader',
+							options: {
+								sourceMap: true
+							}
+						},
+						{
+							loader: 'postcss-loader',
+							options: postcssConfig
+						},
+						{
+							loader: 'sass-loader',
+							options: {
+								sourceMap: true
+							}
+						}
+					],
+					fallback: 'style-loader'
+				})
+			},
+			{
+				test: /\.png$/,
+				loaders: ['file-loader?name=../../[path][name].[ext]']
+			},
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: babelConfig
+			}
+		]
+	},
+	plugins: [
+		new ExtractTextPlugin({
+			filename: paths.stylesBuild,
+			allChunks: true
+		}),
+		new SpritesmithPlugin({
+			src: {
+				cwd: path.resolve(__dirname, paths.iconsSrc),
+				glob: '*.png'
+			},
+			target: {
+				image: path.resolve(__dirname, paths.iconsTarget),
+				css: path.resolve(__dirname, paths.iconsStyle)
+			},
+			apiOptions: {
+				cssImageRef: paths.iconsRef
+			}
+		}),
+		new CleanWebpackPlugin(paths.cleanUp, {
+			verbose: false
+		})
+	],
+	cache: true,
+	bail: false,
+	devtool: 'source-map'
 };
 
 module.exports = env => {
-  if (env.NODE_ENV === 'development') {
-    config.plugins.push(
-      new BrowserSyncPlugin({
-        host: 'localhost',
-        port: 3000,
-        open: 'external',
-        files: [
-          '*.php',
-          '*.html',
-          './assets/styles/app.css',
-          './assets/styles/app.js'
-        ],
-        ghostMode: {
-          clicks: false,
-          scroll: true,
-          forms: {
-            submit: true,
-            inputs: true,
-            toggles: true
-          }
-        },
-        snippetOptions: {
-          rule: {
-            match: /<\/body>/i,
-            fn: (snippet, match) => `${snippet}${match}`
-          }
-        },
-        proxy: 'localhost'
-      })
-    );
-  }
+	if (env.NODE_ENV === 'development') {
+		config.plugins.push(
+			new BrowserSyncPlugin({
+				host: 'localhost',
+				port: 3000,
+				open: 'external',
+				files: [
+					'*.php',
+					'*.html',
+					'./assets/styles/app.css',
+					'./assets/styles/app.js'
+				],
+				ghostMode: {
+					clicks: false,
+					scroll: true,
+					forms: {
+						submit: true,
+						inputs: true,
+						toggles: true
+					}
+				},
+				snippetOptions: {
+					rule: {
+						match: /<\/body>/i,
+						fn: (snippet, match) => `${snippet}${match}`
+					}
+				},
+				proxy: 'localhost'
+			})
+		);
+	}
 
-  if (env.NODE_ENV === 'production') {
-    config.plugins.push(
-      new UglifyJSPlugin({
-        sourceMap: true
-      })
-    );
-  }
+	if (env.NODE_ENV === 'production') {
+		config.plugins.push(
+			new UglifyJSPlugin({
+				sourceMap: true
+			})
+		);
+	}
 
-  return config;
+	return config;
 };
