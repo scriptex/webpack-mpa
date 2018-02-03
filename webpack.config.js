2139329cf68c82cf890460566e2bdf09ce3ed6e2
Atanas Atanasov <scriptex.bg@gmail.com>
2018-02-03 12:58:51 +0200

    Update file loader's regex

--- webpack.config.js
+++ webpack.config.js
@@ -198,7 +198,7 @@ const config = {
 				use: babelConfig
 			},
 			{
-				test: /\.(jpe?g|gif|[^sprite\.]png|svg|woff2?|ttf|eot|wav|mp3|mp4)(\?.*$|$)/,
+				test: /\.(jpe?g|gif|png|svg|woff2?|ttf|eot|wav|mp3|mp4)(\?.*$|$)/,
 				use: [
 					{
 						loader: 'file-loader',
