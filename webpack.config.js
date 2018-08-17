515ea3a69edd14b242c610b72739dfbab7c8b80d
Atanas Strahilov <atanass@eondata.onmicrosoft.com>
2018-08-17 14:53:05 +0300

    Bump dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -58,10 +58,20 @@ const babelConfig = [
 		options: {
 			cacheDirectory: true,
 			comments: false,
-			presets: [
-				'@babel/env',
-				['@babel/stage-2', { decoratorsLegacy: true }]
-			]
+			presets: ['@babel/env'],
+			plugins: [
+				// Stage 2
+				['@babel/plugin-proposal-decorators', { 'legacy': true }],
+				'@babel/plugin-proposal-function-sent',
+				'@babel/plugin-proposal-export-namespace-from',
+				'@babel/plugin-proposal-numeric-separator',
+				'@babel/plugin-proposal-throw-expressions',
+				// Stage 3
+				'@babel/plugin-syntax-dynamic-import',
+				'@babel/plugin-syntax-import-meta',
+				['@babel/plugin-proposal-class-properties', { 'loose': false }],
+				'@babel/plugin-proposal-json-strings'
+			  ]
 		}
 	}
 ];
