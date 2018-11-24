a2e6e36202a76642a40c5e3f04da4a041a823fb2
Atanas Atanasov <scriptex@users.noreply.github.com>
2018-11-24 14:28:26 +0200

    Update cli.js

--- cli.js
+++ cli.js
@@ -25,32 +25,13 @@ const cwd = join(__dirname, './');
  */
 const filesToSkip = ['cli.js'];
 
-/**
- * Determine if a file should be skipped
- *
- * @param {string} path
- *
- * @return {boolean}
- */
-const shouldSkip = path => {
-	const filename = path.split(sep).pop();
-	return filesToSkip.indexOf(filename) > -1;
-};
-
-/**
- * Filter files which should be skipped
- *
- * @param {string} name
- *
- * @return {boolean}
- */
-const filter = name => {
-	const isDotFile = name.indexOf(`${sep}.`) > -1;
-	const isNodeModule = name.indexOf('node_modules') > -1;
-
-	return !isDotFile && !isNodeModule && !shouldSkip(name);
-};
-
 ncp.limit = 16;
 
-ncp(cwd, pwd, { filter }, error => console.log(error || 'Webpack MPA installed'));
+ncp(
+	cwd,
+	pwd,
+	{
+		filter: path => filesToSkip.indexOf(path.split(sep).pop()) === -1
+	},
+	error => console.log(error || 'Webpack MPA installed')
+);
