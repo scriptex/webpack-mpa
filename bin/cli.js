1f0c02bf52ced4f44c5caaaa61c2fa5b8e5a196b
Atanas Atanasov <scriptex@users.noreply.github.com>
2019-05-29 22:07:12 +0300

    Update cli.js

--- bin/cli.js
+++ bin/cli.js
@@ -10,7 +10,7 @@ const { join } = require('path');
  */
 const { copyDir } = require('./copy');
 
-const shouldSkip = name => name === 'node_modules' || name === 'bin';
+const shouldSkip = name => name === 'node_modules' || name === 'bin' || name === '.github';
 
 copyDir(join(__dirname, '../'), process.env.PWD, shouldSkip);
 
