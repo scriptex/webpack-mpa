4abb25d18ea34c3c01ff7293845e6ed2be7b7d8b
Atanas Atanasov <scriptex.bg@gmail.com>
2021-03-10 15:32:26 +0200

    Fix issue with path being undefined on Windows

--- bin/cli.js
+++ bin/cli.js
@@ -12,6 +12,6 @@ const { copyDir } = require('./copy');
 
 const shouldSkip = name => name === 'node_modules' || name === 'bin' || name === '.github';
 
-copyDir(join(__dirname, '../'), process.env.PWD, shouldSkip);
+copyDir(join(__dirname, '../'), process.cwd(), shouldSkip);
 
 console.log('Webpack MPA is now setup! Run "npm i" or "yarn" to continue');
