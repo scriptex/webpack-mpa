d6266a13b88dd7ed51febde3be9dbd448c3bfe8b
Atanas Atanasov <scriptex.bg@gmail.com>
2026-06-04 14:23:50 +0300

    Update all dependencies (#1075)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,19 +2,12 @@ import globals from 'globals';
 
 import compat from 'eslint-plugin-compat';
 
-const GLOBALS_BROWSER_FIX = Object.assign({}, globals.browser, {
-	AudioWorkletGlobalScope: globals.browser['AudioWorkletGlobalScope ']
-});
-
-delete GLOBALS_BROWSER_FIX['AudioWorkletGlobalScope '];
-
 export default [
 	{
 		files: ['./assets/scripts/**/*.js'],
 		languageOptions: {
 			globals: {
-				...globals.node,
-				...GLOBALS_BROWSER_FIX
+				...globals.node
 			},
 			parserOptions: {
 				ecmaVersion: 2023,
